5a56165316b5751e0bdcf4812273e785bf9c1aa3
Angelo Fidelis <angelo.fidelis@avenuecode.com>
2025-01-29 15:46:16 -0300

    terminando a configuracao do startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,16 @@
-# Configuraçoes iniciais
+# -------------------------------------------------- Variáveis -----------------------------------------
+PROJECT_ID=lookerstudylab
+SERVICE_ACOUNT_NAME=etl-monday-to-bq
+SERVICE_ACOUNT_VIEWER='extract-data-api-sheet'
+
+# -------------------------------------------- Configuraçoes iniciais -----------------------------------------
 gcloud auth login
+gcloud config set project PROJECT_ID
 
-YOUR_PROJECT_ID=lookerstudylab
-gcloud config set project YOUR_PROJECT_ID
-
-#Criando container local
+# -------------------------------------------- Criando container local -------------------------------------------- 
 docker build -t us-central1-docker.pkg.dev/lookerstudylab/monday/export-monday:v1 .
 
-# Criando o repositorio para o container
+# -------------------------------------- Criando o repositorio para o container -------------------------------------------- 
 gcloud artifacts repositories create monday \
   --repository-format=docker \
   --location=us-central1 \
@@ -15,13 +18,41 @@ gcloud artifacts repositories create monday \
 
 gcloud auth configure-docker us-central1-docker.pkg.dev
 
-# Realizando um upload do container para o repositorio
+# -------------------------------- Realizando um upload do container para o repositorio -------------------------------------------- 
 docker push us-central1-docker.pkg.dev/lookerstudylab/monday/export-monday:v1
+
+# --------------------------------------- Criando as service Accounts -------------------------------------------- 
+
+gcloud iam service-accounts create $SERVICE_ACOUNT_NAME \
+  --display-name="'${SERVICE_ACOUNT_NAME}'" \
+  --project=$PROJECT_ID
+
+gcloud iam service-accounts create $SERVICE_ACOUNT_VIEWER \
+  --display-name="'${SERVICE_ACOUNT_VIEWER}'" \
+  --project=$PROJECT_ID
+
+# ------------------------------- Atribuindo permissoes as services accounts -------------------------------------------- 
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/secretmanager.secretAccessor"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/bigquery.admin"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACOUNT_VIEWER@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/viewer"
+
+# ---------------------------------------- Criando o Cloud Run -------------------------------------------- 
+
+https://cloud.google.com/iam/docs/service-accounts-create?hl=pt-br
 
 gcloud run deploy export-app \
   --image us-central1-docker.pkg.dev/lookerstudylab/monday/export-monday:v1 \
   --region us-central1 \
-  --service-account=access-cloud-run@lookerstudylab.iam.gserviceaccount.com \
+  --service-account=$SERVICE_ACOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com \
   --platform managed \
   --allow-unauthenticated \
   --set-env-vars \
